bd806deb71eb64c9e1da7be6bd67c6a6505aa2e7
scbq <felipe.aguilera.fuentealba@gmail.com>
2024-12-15 21:17:44 -0300

    portafolio

---
+++ js/aos-init.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+      duration: 1000,  // Duración de la animación
+      easing: "ease-in-out",
+      once: true  // La animación ocurre una vez
+    });
+  });
+  
---
+++ libs/aos.js
@@ -0,0 +1,9 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true
+    })
+  });
+</script>
